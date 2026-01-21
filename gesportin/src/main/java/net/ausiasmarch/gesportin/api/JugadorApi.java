667eeac3664db24d69f2150d274263c98700cc05
Vladislav Uski <sunfeax@gmail.com>
2026-01-21 12:38:34 +0100

    Remove 'capitan' parameter from getPage method

--- gesportin/src/main/java/net/ausiasmarch/gesportin/api/JugadorApi.java
+++ gesportin/src/main/java/net/ausiasmarch/gesportin/api/JugadorApi.java
@@ -36,10 +36,9 @@ public class JugadorApi {
     public ResponseEntity<Page<JugadorEntity>> getPage(
             @PageableDefault(size = 1000) Pageable pageable,
             @RequestParam(required = false) String posicion,
-            @RequestParam(required = false) Boolean capitan,
             @RequestParam(required = false) Long idUsuario,
             @RequestParam(required = false) Long idEquipo) {
-        return ResponseEntity.ok(oJugadorService.getPage(pageable, posicion, capitan, idUsuario, idEquipo));
+        return ResponseEntity.ok(oJugadorService.getPage(pageable, posicion, idUsuario, idEquipo));
     }
 
     @PostMapping
